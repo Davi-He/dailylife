b5d5a0e7950b66e2fcbadb4560e7f2312cea8f50
hepeng <hepeng0@gmail.com>
2014-01-15 23:32:02 +0800

    add location

---
+++ where/src/com/test/where/MainActivity.java
@@ -0,0 +1,38 @@
+package com.test.where;
+import android.app.Activity;
+import android.content.Context;
+import android.location.Location;
+import android.location.LocationManager;
+import android.os.Bundle;
+import android.widget.TextView;
+public class MainActivity extends Activity {
+   
+	private TextView longitude;
+	private TextView latitude;
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        
+        longitude = (TextView)findViewById(R.id.longitude);
+        latitude = (TextView)findViewById(R.id.latitude);
+        
+        Location mLocation = getLocation(this);
+        
+        longitude.setText("Longitude: " + mLocation.getLongitude());
+        latitude.setText("Latitude: " + mLocation.getLatitude());
+    }
+    
+    //Get the Location by GPS or WIFI
+	public Location getLocation(Context context) {
+		LocationManager locMan = (LocationManager) context
+				.getSystemService(Context.LOCATION_SERVICE);
+		Location location = locMan
+				.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+		if (location == null) {
+			location = locMan
+					.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
+		}
+		return location;
+	}
+}
